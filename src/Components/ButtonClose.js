49b3266f85ae8d42a81ef14e2a33bd9473f3f352
Mikhail Agapov <mr.hapon@gmail.com>
2023-10-23 19:34:23 +0300

    SetTimeout, Navigate

--- src/Components/ButtonClose.js
+++ src/Components/ButtonClose.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const ButtonClose = () => {
-  return <button className="button-close" />
+  const navigate = useNavigate()
+  return <button onClick={() => navigate('/')} className="button-close" />
 }
 
 export default ButtonClose
--- src/Components/FrameFirstPage.js
+++ src/Components/FrameFirstPage.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import qrcode from '../images/index 1.png'
 import { useNavigate } from 'react-router-dom'
 
 const FrameFirstPage = () => {
   const navigate = useNavigate()
-  return (
+  const [isReady, setIsReady] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsReady(true)
+    }, 5000)
+  }, [])
+  return isReady ? (
     <div className="frame-first-page">
       <div className="ffp-header">
         ИСПОЛНИТЕ МЕЧТУ ВАШЕГО МАЛЫША! ПОДАРИТЕ ЕМУ СОБАКУ!
@@ -13,6 +20,8 @@ const FrameFirstPage = () => {
       <div className="ffp-text">Сканируйте QR-код или нажмите ОК</div>
       <button onClick={() => navigate('/second')}>OK</button>
     </div>
+  ) : (
+    <div></div>
   )
 }
 
